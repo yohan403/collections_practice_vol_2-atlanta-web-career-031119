cf3259ee49c1a537dfe1331bb3e5638d70f2f7a4
John Cho <johnycho16@gmail.com>
2019-03-04 06:09:39 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,26 @@
 # your code goes here
+
+def begins_with_r(array)
+  new_array = []
+  array.each do |i|
+    if i.start_with?("r")
+      new_array << TRUE
+    else
+      new_array << FALSE
+    end
+  end
+  if new_array.include?(FALSE)
+    return FALSE
+  else
+    return TRUE
+  end
+end
+
+def contain_a(array)
+  new_array = []
+  array.each do |i|
+    if i.include?("a") == TRUE
+      new_array << i
+    end
+  end
+end
